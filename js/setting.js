d95d0229633a04bbcb48ab30d6f61119eb77973b
Jizai2012ikusei <jinzai2012ikusei@gmail.com>
2015-07-31 15:51:51 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -109,6 +109,7 @@ OpenSpending.Styles.Cofog = {
   '9401': { icon: 'icons/villding.svg', color: '#00bfff', bcolor: '#D33673' },
   '9402': { icon: 'icons/schools.svg', color: '#00bfff', bcolor: '#D33673' },
   '9500': { icon: 'icons/study1.svg', color: '#00bfff', bcolor: '#D33673' },
+  '9501': { icon: 'icons/EC.svg', color: '#00bfff', bcolor: '#D33673' },
   '9600': { icon: 'icons/sports.svg', color: '#00bfff', bcolor: '#D33673' },
   '9602': { icon: 'icons/shefu.svg', color: '#00bfff', bcolor: '#D33673' },
   '9700': { icon: 'icons/society.svg', color: '#00bfff', bcolor: '#D33673' },
